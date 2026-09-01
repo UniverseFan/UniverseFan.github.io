79a750bd06a7045058f3f10902646511b5defaef
张宇凡 <zhangyufan@zhangyufandeMacBook-Air.local>
2026-09-01 15:10:25 +0800

    Configure Astro for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,10 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-import edgeoneAdapter from "@edgeone/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://sorollaportfolio.edgeone.app/",
+  site: "https://universefan.github.io/",
   output: "static",
   integrations: [mdx(), sitemap(), tailwind(), icon()],
-  adapter: edgeoneAdapter()
 });
