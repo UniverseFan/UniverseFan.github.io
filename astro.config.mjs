7103f0b4d422dd48cb77eea99ec848762a3b562e
张宇凡 <zhangyufan@zhangyufandeMacBook-Air.local>
2026-09-01 14:53:28 +0800

    init

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+import edgeoneAdapter from "@edgeone/astro";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://sorollaportfolio.edgeone.app/",
+  output: "static",
+  integrations: [mdx(), sitemap(), tailwind(), icon()],
+  adapter: edgeoneAdapter()
+});
---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Evan.Zhang';
+export const SITE_DESCRIPTION = 'Evan Zhang 的个人空间：玩乐、料理、学习与工作记录。';
